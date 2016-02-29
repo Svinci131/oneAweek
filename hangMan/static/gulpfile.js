e45a7dac166949f3c5396f104e197fba3bef3f0e
Svinci131 <svinci131@gmail.com>
2016-02-29 15:58:43 -0500

    set up react

--- hangMan/static/gulpfile.js
+++ hangMan/static/gulpfile.js
@@ -3,11 +3,12 @@ var source = require('vinyl-source-stream');
 var browserify = require('browserify');
 var uglify = require('gulp-uglify');
 var streamify = require('gulp-streamify');
+var babelify = require('babelify');
 
 gulp.task('browserify', function() {
     return browserify('main.js')
+    	.transform( "babelify", {presets: ["react"]})
         .bundle()
-        
         .pipe( source( 'bundled.js' ) )
         // we take our browserified stream and run it through
         // uglify's minifier
@@ -16,3 +17,6 @@ gulp.task('browserify', function() {
 });
 
 gulp.watch('*', ['browserify']);
+
+//$ npm install --save react react-dom babelify babel-preset-react
+//$ browserify -t [ babelify --presets [ react ] ] main.js -o bundled.js
--- hangMan/static/main.js
+++ hangMan/static/main.js
@@ -1,1 +1,9 @@
-alert("foo")
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+  <h1>Hello!</h1>,
+  document.getElementById('example')
+);
+
+alert("bar")
