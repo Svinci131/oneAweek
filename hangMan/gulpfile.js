29716be373b834c07d3a6316df4b99b5e2292ede
Svinci131 <svinci131@gmail.com>
2016-03-01 15:46:30 -0500

    restructured folder

--- hangMan/gulpfile.js
+++ hangMan/gulpfile.js
@@ -6,17 +6,17 @@ var streamify = require('gulp-streamify');
 var babelify = require('babelify');
 
 gulp.task('browserify', function() {
-    return browserify('main.js')
+    return browserify('javascript/main.js')
     	.transform( "babelify", {presets: ["react"]})
         .bundle()
-        .pipe( source( 'bundled.js' ) )
+        .pipe( source( 'javascript/bundled.js' ) )
         // we take our browserified stream and run it through
         // uglify's minifier
         .pipe( streamify( uglify() ) )
         .pipe( gulp.dest('./') );
 });
 
-gulp.watch('*', ['browserify']);
+gulp.watch('javascript/*', ['browserify']);
 
 //$ npm install --save react react-dom babelify babel-preset-react
 //$ browserify -t [ babelify --presets [ react ] ] main.js -o bundled.js
