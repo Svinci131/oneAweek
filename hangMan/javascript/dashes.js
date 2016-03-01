22db55ac3999a1f1aa66340c509571d1e81f4e69
Svinci131 <svinci131@gmail.com>
2016-03-01 17:30:30 -0500

    added dashed

--- hangMan/javascript/dashes.js
+++ hangMan/javascript/dashes.js
@@ -2,6 +2,16 @@ var React = require('react');
 
 module.exports = React.createClass({
 	render:function(){
-		return (<h1>dashes</h1>)
+		var word = this.props.data.word;
+		
+		return (<div>{word.map(function(l){
+			var isGuessed = this.props.data.lettersShown()[l]
+			if (!isGuessed) {
+				return (<span className='blank' >_</span>)
+			}
+			else {
+				return (<span className='blank' >{l}</span>)
+			}
+		}.bind(this))}</div>)
 	}
 });
--- hangMan/javascript/main.js
+++ hangMan/javascript/main.js
@@ -16,29 +16,64 @@ ReactDOM.render(
 //model 
 var Model = {
 	//keys guessed 
-	word: "flowers",
+	word: "flowers".split(''),
+	lettersShown: function(){
+		var word = Model.word;
+		var obj = {};
+		for (var i = word.length-1; i >=0 ; i--){
+			obj[word[i]] = false;
+		}
+		return obj
+	},
+	numGuesses: 7,
 	keysGuessed: []
 }
 //Dashes
 //KeyBoard 
+//draw letters if this.props.data.keysGuessed[letter] !== undefined - add attr disabled
+//onClick add to keysGuessed
+//check if its in the word 
+//if it is update dashes 
+//if not decrease number of guesses 
+
 ee.on('keyClicked', function (letter) {
 	Model.keysGuessed.push(letter)
-	console.log("foo",Model.keysGuessed)
-	render();
+	//console.log("foo",Model.keysGuessed)
+	check (letter)
+	
 });
 
 
-render() 
-
+render(); 
 
 function render() {
 	ReactDOM.render(
 	  <KeyBoard data={Model} ee={ee} />,
 	  document.getElementById('keyBoard')
 	);
+	ReactDOM.render(
+		<Dashes data={Model} />,
+		document.getElementById('dashes'));
+	//render dashes here to 
 }
 
-
+function check (l) {
+	var word = Model.word;
+	var isRight = false; 
+	// console.log(Model.lettersShown()[s])
+  //for (var i = word.length-1; i >=0 ; i--){
+  	// if (word[i] === l) {
+  		//isRight = true; 
+  		//}
+  //}
+  	//if (!isRight) {
+  			//Model.numGuesses -= 1
+  		//} 
+  	// else {
+  		//Model.lettersShown()[l.toLowerCase()] = true;
+  	// }
+  	render();
+}
 
 
 
